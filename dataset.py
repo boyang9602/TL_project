d29160d152e214e39ce7986fbcb0fd2500c68e53
Bo Yang <boyang9602@gmail.com>
2023-07-12 17:29:21 -0400

    added the code to generate filelist.txt and tested the datasets

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import cv2
 import json
 import torch
@@ -92,3 +93,42 @@ class S2TLD1080Dataset(torch.utils.data.Dataset):
     @staticmethod
     def item_shape():
         return 1080, 1920, 3
+    
+if __name__ == '__main__':
+    """
+    running this script directly will generate the filelist.txt for both S2TLD720 and S2TLD1080
+    sys.argv[1] is the root folder of S2TLD720
+    sys.argv[2] is the root folder of S2TLD1080
+    """
+
+    # 720
+    with open(f'{sys.argv[1]}/filelist.txt', 'w') as f:
+        nonexistence = [1697, 1908, 2950]
+        for idx in range(4564):
+            cursor = 0
+            for i, case in enumerate(nonexistence):
+                if idx + i >= case:
+                    cursor += 1
+            folder = 'normal_1' if idx <= 778 else 'normal_2'
+            f.write('{},{:06d}\n'.format(folder, idx + cursor))
+    # 1080
+    files = sorted(os.listdir(f'{sys.argv[2]}/JPEGImages/'))
+    with open(f'{sys.argv[2]}/filelist.txt', 'w') as f:
+        for file in files:
+            f.write(file[:-4] + '\n')
+
+    # test
+    # 1. the length is expected
+    # 2. no repeated items in the filelist.txt
+    # 3. all the files can be loaded
+    # 720
+    ds720 = S2TLD720Dataset(sys.argv[1])
+    assert len(ds720) == 4564
+    for i in range(len(ds720)):
+        _ = ds720[i]
+
+    # 1080
+    ds1080 = S2TLD1080Dataset(sys.argv[2])
+    assert len(ds1080) == 1222
+    for i in range(len(ds1080)):
+        _ = ds1080[i]
