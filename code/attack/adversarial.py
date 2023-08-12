3b40fe1e0d0e6e36fb1af3cf9678d3afae1a8685
Bo Yang <boyang9602@gmail.com>
2023-08-12 16:45:03 -0400

    separate the objective, losses from the adversarial function

--- code/attack/adversarial.py
+++ code/attack/adversarial.py
@@ -1,6 +1,4 @@
 import torch
-import torch.nn.functional as F
-from tools.utils import IoU_multi, IoG_single
 
 def process_adv_img(img, adv_img, eps):
     """project the perturbations, not exceeds -eps and eps, new img not exceed 0-255"""
@@ -10,15 +8,7 @@ def process_adv_img(img, adv_img, eps):
     adv_img = img + perturbation
     return adv_img
 
-def naive_tltype_identifier(gt_box):
-    if gt_box[3] - gt_box[1] > gt_box[2] - gt_box[0]:
-        # height > width, vertical
-        return 1
-    else:
-        # else horizontal
-        return 3
-
-def adversarial(model, data_item, objective_fn, step_size=3, eps=16, budget=5, device=None):
+def adversarial(model, data_item, objective_fn, step_size=3, eps=16, budget=5):
     """
     This is the overall framework
     1. init the perturbation
@@ -56,67 +46,3 @@ def adversarial(model, data_item, objective_fn, step_size=3, eps=16, budget=5, d
         iter_num += 1
         adv_img = process_adv_img(adv_img.detach().clone(), adv_img.detach().clone(), eps).requires_grad_()
     return adv_img
-
-def objective(boxes, colors, inferred_tl_types, output, loss_fns):
-    """
-    Basic objective. 
-    It will apply the loss function on the detections having intersection with the ground truths
-    """
-    valid_detections, recognitions, assignments, invalid_detections, rpn_data, rcnn_boxes, rcnn_scores = output
-
-    score = 0
-    # process valid_detections
-    valid_ious = IoU_multi(valid_detections[:, 1:5], boxes)
-    maxes = torch.max(valid_ious, 1)
-    for det_idx, (gt_idx, iou) in enumerate(zip(maxes.indices, maxes.values)):
-        if iou <= 1e-4:
-            # no intersection, pass
-            continue
-        score += loss_fns['rcnn_reg_loss'](boxes[gt_idx], valid_detections[det_idx][1:5], iou)
-        score += loss_fns['rcnn_cls_loss'](inferred_tl_types[gt_idx], valid_detections[det_idx][5:])
-        score += loss_fns['rec_cls_loss'](colors[gt_idx], recognitions[det_idx])
-
-    # process invalid_detections
-    invalid_ious = IoU_multi(invalid_detections[:, 1:5], boxes)
-    maxes = torch.max(invalid_ious, 1)
-    for det_idx, (gt_idx, iou) in enumerate(zip(maxes.indices, maxes.values)):
-        if iou <= 1e-4:
-            # no intersection, pass
-            continue
-        score += loss_fns['rcnn_reg_loss'](boxes[gt_idx], invalid_detections[det_idx][1:5], iou)
-        score += loss_fns['rcnn_cls_loss'](inferred_tl_types[gt_idx], invalid_detections[det_idx][5:])
-
-    # process RPN layer intermediate data
-    rpn_ious = IoU_multi(rpn_data[:, 1:5], boxes)
-    maxes = torch.max(rpn_ious, 1)
-    for det_idx, (gt_idx, iou) in enumerate(zip(maxes.indices, maxes.values)):
-        if iou <= 1e-4:
-            # no intersection, pass
-            continue
-        score += loss_fns['rpn_reg_loss'](boxes[gt_idx], rpn_data[det_idx][1:5], iou)
-        score += loss_fns['rpn_cls_loss'](1, rpn_data[det_idx][5:])
-
-    return score
-
-def dummy_loss(*args):
-    return 0
-
-def box_smooth_l1_loss(gt_box, det_box, iou):
-    # try to make the negative loss of the det_box and gt_box smaller
-    # so that the loss will be bigger, which means det_box and gt_box become 
-    # much more different
-    return -F.smooth_l1_loss(gt_box, det_box)
-
-def box_iou_loss(gt_box, det_box, iou):
-    # try to make the iou smaller
-    return iou
-
-def box_iog_loss(gt_box, det_box, iou):
-    # try to make the iog smaller
-    return IoG_single(gt_box, det_box)
-
-def cls_nll_loss(gt_idx, scores_vec):
-    return -F.nll_loss(scores_vec.unsqueeze(0), torch.tensor([gt_idx], device=scores_vec.device))
-
-def cls_gt_score_loss(gt_idx, scores_vec):
-    return scores_vec[gt_idx]
--- code/attack/nontarget_experiment.py
+++ code/attack/nontarget_experiment.py
@@ -1,11 +1,13 @@
-import torch
+import os
+import time
 import pickle
+import argparse
+import torch
+import torch.nn.functional as F
+from tools.utils import IoU_multi, IoG_single
 from models.pipeline4attack import load_pipeline
 from tools.dataset import get_dataset
 import attack.adversarial as adversarial
-import argparse
-import os
-import time
 
 TL_TYPES = ['UNK', 'VERT', 'QUAD', 'HORI']
 COLOR_LABELS = ["off", "red", "yellow", "green"]
@@ -29,19 +31,79 @@ def load_topk_idxs(filename):
                 topk.append(item[0])
     return topk
 
-def get_loss_fns(rcnn_reg_loss_name, rcnn_cls_loss_name, rec_cls_loss_name, rpn_reg_loss_name, rpn_cls_loss_name):
-    loss_fns = {
-        'rcnn_reg_loss': getattr(adversarial, rcnn_reg_loss_name),
-        'rcnn_cls_loss': getattr(adversarial, rcnn_cls_loss_name),
-        'rec_cls_loss': getattr(adversarial, rec_cls_loss_name),
-        'rpn_reg_loss': getattr(adversarial, rpn_reg_loss_name),
-        'rpn_cls_loss': getattr(adversarial, rpn_cls_loss_name)
-    }
-    return loss_fns
+def objective(boxes, colors, inferred_tl_types, output, loss_fns):
+    """
+    Basic objective. 
+    It will apply the loss function on the detections having intersection with the ground truths
+    """
+    valid_detections, recognitions, assignments, invalid_detections, rpn_data, rcnn_boxes, rcnn_scores = output
 
-if __name__ == '__main__':
-    torch.manual_seed(42)
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    score = 0
+    # process valid_detections
+    valid_ious = IoU_multi(valid_detections[:, 1:5], boxes)
+    maxes = torch.max(valid_ious, 1)
+    for det_idx, (gt_idx, iou) in enumerate(zip(maxes.indices, maxes.values)):
+        if iou <= 1e-4:
+            # no intersection, pass
+            continue
+        score += loss_fns['rcnn_reg_loss'](boxes[gt_idx], valid_detections[det_idx][1:5], iou)
+        score += loss_fns['rcnn_cls_loss'](inferred_tl_types[gt_idx], valid_detections[det_idx][5:])
+        score += loss_fns['rec_cls_loss'](colors[gt_idx], recognitions[det_idx])
+
+    # process invalid_detections
+    invalid_ious = IoU_multi(invalid_detections[:, 1:5], boxes)
+    maxes = torch.max(invalid_ious, 1)
+    for det_idx, (gt_idx, iou) in enumerate(zip(maxes.indices, maxes.values)):
+        if iou <= 1e-4:
+            # no intersection, pass
+            continue
+        score += loss_fns['rcnn_reg_loss'](boxes[gt_idx], invalid_detections[det_idx][1:5], iou)
+        score += loss_fns['rcnn_cls_loss'](inferred_tl_types[gt_idx], invalid_detections[det_idx][5:])
+
+    # process RPN layer intermediate data
+    rpn_ious = IoU_multi(rpn_data[:, 1:5], boxes)
+    maxes = torch.max(rpn_ious, 1)
+    for det_idx, (gt_idx, iou) in enumerate(zip(maxes.indices, maxes.values)):
+        if iou <= 1e-4:
+            # no intersection, pass
+            continue
+        score += loss_fns['rpn_reg_loss'](boxes[gt_idx], rpn_data[det_idx][1:5], iou)
+        score += loss_fns['rpn_cls_loss'](1, rpn_data[det_idx][5:])
+
+    return score
+
+def naive_tltype_identifier(gt_box):
+    if gt_box[3] - gt_box[1] > gt_box[2] - gt_box[0]:
+        # height > width, vertical
+        return 1
+    else:
+        # else horizontal
+        return 3
+
+def dummy_loss(*args):
+    return 0
+
+def box_smooth_l1_loss(gt_box, det_box, iou):
+    # try to make the negative loss of the det_box and gt_box smaller
+    # so that the loss will be bigger, which means det_box and gt_box become 
+    # much more different
+    return -F.smooth_l1_loss(gt_box, det_box)
+
+def box_iou_loss(gt_box, det_box, iou):
+    # try to make the iou smaller
+    return iou
+
+def box_iog_loss(gt_box, det_box, iou):
+    # try to make the iog smaller
+    return IoG_single(gt_box, det_box)
+
+def cls_nll_loss(gt_idx, scores_vec):
+    return -F.nll_loss(scores_vec.unsqueeze(0), torch.tensor([gt_idx], device=scores_vec.device))
+
+def cls_gt_score_loss(gt_idx, scores_vec):
+    return scores_vec[gt_idx]
+
+def handle_args():
     parser = argparse.ArgumentParser(description='Run the non target attacks')
     parser.add_argument('--dataset', '-ds', action='store', required=True, help='the dataset.')
     parser.add_argument('--topk_file', '-f', action='store', required=False, default=None, help='the selected perfect cases.')
@@ -57,33 +119,44 @@ if __name__ == '__main__':
     parser.add_argument('--rpn_reg_loss', '-rb', action='store', required=False, default='dummy_loss', help='RPN box loss function name')
     parser.add_argument('--rpn_cls_loss', '-o', action='store', required=False, default='dummy_loss', help='RPN layer objectiveness loss function name')
     args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    torch.manual_seed(42)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    args = handle_args()
 
     pl = load_pipeline(device)
     ds = get_dataset(args.dataset, device=device)
+
+    loss_fns = {
+        'rcnn_reg_loss': locals()[args.rcnn_reg_loss],
+        'rcnn_cls_loss': locals()[args.rcnn_cls_loss],
+        'rec_cls_loss': locals()[args.rec_cls_loss],
+        'rpn_reg_loss': locals()[args.rpn_reg_loss],
+        'rpn_cls_loss': locals()[args.rpn_cls_loss]
+    }
 
     def objective_fn(data_item, output):
         boxes = torch.tensor(data_item['boxes'], device=device)
         colors = data_item['colors']
         inferred_tl_types = data_item['inferred_tl_types']
         assert 'NA' not in inferred_tl_types
-        loss_fns = get_loss_fns(args.rcnn_reg_loss, args.rcnn_cls_loss, args.rec_cls_loss, args.rpn_reg_loss, args.rpn_cls_loss)
-        return adversarial.objective(boxes, convert_labels_to_nums(colors, COLOR_LABELS), convert_labels_to_nums(inferred_tl_types, TL_TYPES), output, loss_fns)
+        return objective(boxes, convert_labels_to_nums(colors, COLOR_LABELS), convert_labels_to_nums(inferred_tl_types, TL_TYPES), output, loss_fns)
 
-    adv_imgs = []
     topk_filename = args.topk_file
     if topk_filename is None:
         topk_filename = f'data/evaluation/{args.dataset}_top200.bin'
-    
-    
+
+    adv_imgs = []
     t1 = time.perf_counter()
     for i, idx in enumerate(load_topk_idxs(topk_filename)):
+        if i > 5: break
         data_item = ds[idx]
-        
-        adv_img = adversarial.adversarial(pl, data_item, objective_fn, step_size=args.step_size, eps=args.eps, budget=args.max_iter, device=device)
+        adv_img = adversarial.adversarial(pl, data_item, objective_fn, step_size=args.step_size, eps=args.eps, budget=args.max_iter)
         adv_imgs.append(adv_img.type(torch.uint8).cpu())
     t2 = time.perf_counter()
     print(f"Run all the cases in {t2-t1:0.4f} seconds!")
-
     adv_imgs = torch.stack(adv_imgs)
 
     if args.path is not None:
